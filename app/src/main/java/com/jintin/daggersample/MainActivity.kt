a1f83835f7ab5ab47e3e0193fff737112708bd5e
Jintin <jintin.lin1018@gmail.com>
2020-11-19 07:19:49 +0800

    basic example

--- app/src/main/java/com/jintin/daggersample/MainActivity.kt
+++ app/src/main/java/com/jintin/daggersample/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.jintin.daggersample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val honeyLemonade = HoneyLemonade(Honey(Bee()), Lemon())
+        textView.text = honeyLemonade.toString()
     }
 }
